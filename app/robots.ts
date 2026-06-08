85e28ec829e8c3c545d2a39146700c2cafca4aa3
pjs8875 <pjs8875@gmail.com>
2026-06-08 09:12:49 -0400

    fix: allow Googlebot to crawl /services/ and /markets/ so 301 redirects are visible

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/services/", "/markets/"],
       },
     ],
     sitemap: `${base}/sitemap.xml`,
