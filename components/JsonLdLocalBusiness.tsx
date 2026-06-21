d878f66d6f3a30759652f893ee1076c1e8784d26
pjs8875 <pjs8875@gmail.com>
2026-06-21 13:05:52 -0400

    Add JsonLdLocalBusiness component (LocalBusiness JSON-LD for AI/SEO)

---
+++ components/JsonLdLocalBusiness.tsx
@@ -0,0 +1,19 @@
+export default function JsonLdLocalBusiness() {
+  const data = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "@id": "https://www.coolvulongisland.com/#business",
+    "name": "CoolVu of Long Island",
+    "image": "https://www.coolvulongisland.com/og-image.png",
+    "url": "https://www.coolvulongisland.com",
+    "telephone": "+1-516-535-9555",
+    "email": "paul.silverman@coolvu.com",
+    "priceRange": "$$",
+    "address": { "@type": "PostalAddress", "streetAddress": "1055 Old Country Rd", "addressLocality": "Westbury", "addressRegion": "NY", "postalCode": "11590", "addressCountry": "US" },
+    "areaServed": [ { "@type": "AdministrativeArea", "name": "Nassau County, NY" }, { "@type": "AdministrativeArea", "name": "Suffolk County, NY" } ],
+    "knowsAbout": ["solar window film","privacy window film","safety and security window film","decorative window film","commercial window film"],
+    "aggregateRating": { "@type": "AggregateRating", "ratingValue": "5.0", "reviewCount": "31" },
+    "sameAs": ["https://www.facebook.com/coolvusolutions/","https://www.instagram.com/coolvusolutions","https://x.com/CoolVuSolutions"]
+  };
+  return <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }} />;
+}
