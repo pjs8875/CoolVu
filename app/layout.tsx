70aac92c689c5b450496612fd7d35602a2ca7d38
Paul Silverman <paul.silverman@coolvu.com>
2026-07-18 08:07:52 -0400

    feat: install Meta Pixel (2415713502505318) for CoolVu of Long Island ad account

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,8 +101,31 @@ export default function RootLayout({
             gtag('config', 'G-SSX9WMGTHC', { debug_mode: true });
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2415713502505318');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col font-sans text-coolvu-dark-blue bg-coolvu-off-white">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2415713502505318&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <JsonLdLocalBusiness />
         <script
           type="application/ld+json"
